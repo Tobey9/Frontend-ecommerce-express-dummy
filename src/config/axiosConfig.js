040f9e4356aef8ac45d1710e4a65d221c78061fe
Toby <latebloomerguy99@gmail.com>
2025-05-08 19:33:33 +0700

    fix axiosconfig.js

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // your backend URL
+  baseURL: API_URL, // your backend URL
   withCredentials: true, // important: sends cookies with every request
 });
 
